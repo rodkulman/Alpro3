fd4bd18f662cc1faeb52b73ce54810fec157c598
rodkulman <rodkulman@gmail.com>
2015-05-31 14:12:44 -0300

    Created basic stuff to work with. Added testing project and test-cases.

---
+++ Tabralho2/pucrs.TelDor.Testing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pucrs.TelDor;
+
+namespace pucrs.TelDor.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Tabralho2/pucrs.TelDor/Algorithms.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace pucrs.TelDor
+{
+    public static class Algorithms
+    {
+        public static IEnumerable<int> DoWork(params int[] numbers)
+        {
+            var groups = numbers.Select(i => ToBase(i, 6)).OrderBy(s => s).GroupBy(s => s.Length);
+
+            foreach (var group in groups)
+            {
+                for (int i = 0; i < group.Count() - 1; i++)
+                {
+
+                }
+            }
+        }
+
+        #region Base Conversion
+
+        private static string ToBase(long number, int workingBase)
+        {
+            var retVal = string.Empty;
+
+            while (number >= workingBase)
+            {
+                var mod = number % workingBase;
+                retVal += mod == 10 ? "A" : mod.ToString();
+
+                number = number / workingBase;
+            }
+
+            retVal += number == 10 ? "A" : number.ToString();
+
+            return new string(retVal.Reverse().ToArray());
+        }
+
+        private static long FromBase(string number, int workingBase)
+        {
+            var retVal = 0L;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                retVal += (long)Math.Pow(workingBase, i) * long.Parse(number.Substring(number.Length - 1 - i, 1), NumberStyles.AllowHexSpecifier);
+            }
+
+            return retVal;
+        }
+
+        #endregion
+    }
+}
